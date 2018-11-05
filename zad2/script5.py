b39b90a650a1e008b2e8c28e1a129799c12ef6ae
Monika Stobieniecka <mstobieniecka1@gmail.com>
2018-11-05 23:07:17 +0100

    Added result interpretation

--- zad2/script5.py
+++ zad2/script5.py
@@ -29,7 +29,6 @@ def draw_histogram(data):
         sigma = np.sqrt(variance)
         x = np.linspace(min(arr), max(arr), 100)
         plt.plot(x, mlab.normpdf(x, mean, sigma)*5, color=colors[by_sepal_width.index(arr)]) #uwaga - tutaj mnoze razy piec zeby lepiej bylo widac na histogramie, nie wiem czy to jest ok 
-    
 
     plt.hist(by_sepal_width, bins=25, color=colors, label=labels)                                                                    
     plt.legend()
@@ -43,10 +42,13 @@ def test_hypothesis(data):
     print('Badam hipoteze czy można użyć parametru "sepal width" do rozróżnienia klas kwiatów Iris Versicolour oraz Iris Virginica z poziom istotności statystycznej 5%')
     versicolour = extract_one_species(data, 'Iris-versicolor')
     virginica = extract_one_species(data, 'Iris-virginica')
-    result = stats.ttest_ind(find_column_elements(versicolour, 1), find_column_elements(virginica, 1))
-    print(result)
-
-    #TODO interpret results
+    value, pvalue = stats.ttest_ind(find_column_elements(versicolour, 1), find_column_elements(virginica, 1))
+    print(value, pvalue)
+    # Hipoteza zerowa: nie można można rozróżnić zbiorów na podstawie parametru "sepal width", hipoteza alternatywna: można rozróżnić zbiory na podstawie parametru "sepal width"
+    if pvalue < 0.05:
+	    print('Odrzucamy hipotezę zerową - można rozróżnić zbiory na podstawie parametru "sepal width"')
+    else:
+	    print('Nie ma podstaw do odrzucenia hipotezy zerowej - nie można można rozróżnić zbiorów na podstawie parametru "sepal width"')
 
 if __name__ == '__main__':
   main()
